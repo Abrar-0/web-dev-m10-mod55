1115037a2375e4133b3e0dbe0de2bb2ca4fff99b
Abrar Zawad <abrarzawad222@gmail.com>
2024-04-27 18:21:10 +0600

    updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 // Zcfj73vv2vWRtqzM
 
 
-const uri = "mongodb+srv://abrarzawad1912691042:Zcfj73vv2vWRtqzM@cluster0.fht8ati.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
+// const uri = "mongodb+srv://abrarzawad1912691042:Zcfj73vv2vWRtqzM@cluster0.fht8ati.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
