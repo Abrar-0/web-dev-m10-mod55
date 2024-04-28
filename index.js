0643d65b30b6ea15e6d26c1739e5f2503516e3a0
Abrar Zawad <abrarzawad222@gmail.com>
2024-04-28 10:54:09 +0600

    fixed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
@@ -7,11 +8,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-// abrarzawad1912691042
-// Zcfj73vv2vWRtqzM
-
-
-// const uri = "mongodb+srv://abrarzawad1912691042:Zcfj73vv2vWRtqzM@cluster0.fht8ati.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.fht8ati.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`; 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
